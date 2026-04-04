bd88179a9cfbedb735e672ab5b4730d9cfb2f7fa
Sangeetha-2001 <sangeesangeetha574@gmail.com>
2026-04-04 13:43:07 +0530

    Add files via upload

---
+++ LibraryManagement.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class LibraryManagement {
+
+    
+    public static void main(String[] args) {
+      Library library =  new Library();
+      library.addBook("PonniyinSelvan");
+      library.issueBook();
+
+    }
+
+}
